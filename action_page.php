c085abb86949f1b534a10952c9f9043da42bb7a7
Pans <evantran06@gmail.com>
2022-12-19 13:30:04 -0500

    Created upload page & basic captions

---
+++ action_page.php
@@ -0,0 +1,21 @@
+<html>
+<script src="script.js"></script>
+<link href="style.css" rel="stylesheet" type="text/css" />
+
+<div class="sidebar">
+    <a href="./index.html">Explore</a>
+    <a href="#">Tags</a>
+    <a href="./upload.html" class="disabled-link">Upload</a>
+</div>
+<div class="main">
+    <div class="header">
+        Upload Image
+        <input type="text" placeholder="Search..">
+    </div>
+    <div class="body">
+        <p>Upload image here:</p>
+         
+        <p>Submitted</p>
+    </div>
+</div>
+</html>
