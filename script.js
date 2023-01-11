3bb9dc979f037ffd0ee2636ff5a95f3b7cbde0ac
Pans <evantran06@gmail.com>
2023-01-11 13:06:13 -0500

    Added image referencing from local folder

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ function imgListener(){
     const div = document.getElementById('2');
     div.textContent = lastSplit;
 }
+
+var folder = "images/";
+
+$.ajax({
+    url : folder,
+    success: function (data) {
+        $(data).find("a").attr("href", function (i, val) {
+            if( val.match(/\.(jpe?g|png|gif)$/) ) { 
+                $(".body").append( "<img src='"+ folder + val +"'>" );
+                console.log("test")
+            } 
+        });
+    }
+});
