aa1f193961158920dc30f71e010edddefccc8ea7
Pans <evantran06@gmail.com>
2023-01-11 23:17:19 -0500

    Removed unneeded files

--- Untitled-1.php
+++
@@ -1,7 +0,0 @@
-$directoryName = "images";
-    if(is_writable($directoryName){ 
-        echo "Directory is writable"; // Returns TRUE if it's writable.
-    } 
-    else{ 
-
-} 
--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-$directoryName = "images";
-
-if(is_writable($directoryName)){ 
-    echo 'directory is writable';
-}else{ 
-    echo 'test';
-}
-?>
